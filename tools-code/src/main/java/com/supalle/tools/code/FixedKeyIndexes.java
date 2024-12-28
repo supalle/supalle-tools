3097b2474a41aaeafb41b745c6fb2ce19242ea5a
Supalle <supalle@qq.com>
2024-12-29 01:39:58 +0800

    normalizeKeys

--- tools-code/src/main/java/com/supalle/tools/code/FixedKeyIndexes.java
+++ tools-code/src/main/java/com/supalle/tools/code/FixedKeyIndexes.java
@@ -1,37 +1,61 @@
 package com.supalle.tools.code;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 固定Key的索引
  *
  * @param <K> key的类型
  */
-public class FixedKeyIndexes<K> extends HashMap<K, Integer> {
+public class FixedKeyIndexes<K> extends LinkedHashMap<K, Integer> {
     public static final FixedKeyIndexes<?> EMPTY = new FixedKeyIndexes<>();
 
     public FixedKeyIndexes() {
     }
 
-    public FixedKeyIndexes(Set<K> keySet) {
-        super(Objects.requireNonNull(keySet).size(), 1F);
+    public FixedKeyIndexes(Collection<K> keys) {
+        super((keys = normalizeKeys(keys)).size(), 1F);
+        if (keys.isEmpty()) {
+            return;
+        }
+
         int index = 0;
-        for (K key : keySet) {
+        for (K key : keys) {
             super.put(key, index++);
         }
     }
 
+    /**
+     * 规范化 Keys
+     *
+     * @param keys 键集合
+     * @param <K>  键的类型
+     * @return 规范化后的键集合
+     */
+    public static <K> List<K> normalizeKeys(Collection<K> keys) {
+        if (keys == null || keys.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return keys.stream().filter(Objects::nonNull).distinct().collect(Collectors.toList());
+    }
+
     @SuppressWarnings("unchecked")
-    public static <K> FixedKeyIndexes<K> of(Set<K> keySet) {
-        if (keySet == null || keySet.isEmpty()) {
+    public static <K> FixedKeyIndexes<K> of(Collection<K> keys) {
+        if (keys == null || keys.isEmpty()) {
             return (FixedKeyIndexes<K>) EMPTY;
         }
-        return new FixedKeyIndexes<>(keySet);
+        return new FixedKeyIndexes<>(keys);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <K> FixedKeyIndexes<K> of(K... keys) {
+        if (keys == null || keys.length == 0) {
+            return (FixedKeyIndexes<K>) EMPTY;
+        }
+        return new FixedKeyIndexes<>(Arrays.asList(keys));
     }
 
     public Integer put(K key, Integer value) {
